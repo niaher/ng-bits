48bb478f0a225c814bee111dbd0b508dbbd70914
niaher <niaher.crimston@gmail.com>
2014-11-27 17:09:47 +0700

    Update Polyfill.js

--- ng-bits.Web/ng-bits/Polyfill.js
+++ ng-bits.Web/ng-bits/Polyfill.js
@@ -74,3 +74,7 @@ if (!Array.prototype.forEach) {
 		// 8. return undefined
 	};
 }
+
+String.prototype.trim=function(){return this.replace(/^\s+|\s+$/g, '');};
+String.prototype.ltrim=function(){return this.replace(/^\s+/,'');};
+String.prototype.rtrim=function(){return this.replace(/\s+$/,'');};
