ac4c13705a44ec8a8769289c8c0744de8c9468cf
niaher <niaher.crimston@gmail.com>
2014-12-10 12:39:20 +0700

    Update ngBits.Permissions.js

--- ng-bits.Web/ng-bits/ngBits.Permissions.js
+++ ng-bits.Web/ng-bits/ngBits.Permissions.js
@@ -1,5 +1,5 @@
 angular.module("ngBits.permissions", [])
-	.provider("permissions", function () {
+	.service("permissions", ["$q", function ($q) {
 		var contextlessPermissions = [],
 			contextPermissions = [],
 			pendingPromises = [],
@@ -22,62 +22,59 @@ angular.module("ngBits.permissions", [])
 		this.registerPermissions = function (permissions) {
 			contextlessPermissions = permissions;
 		};
+		
+		this.hasPermission = function(permission, context) {
+			if (angular.isDefined(context)) {
+				var entityType = context.entityType.shortName;
+				var permissionsKey = entityType + "-" + context.entityAspect.getKey().toJSON().values.toString();
+				var permissionArray = contextPermissions[permissionsKey];
 
-		this.$get = ["$q", function ($q) {
-			return {
-				hasPermission: function (permission, context) {
-					if (angular.isDefined(context)) {
-						var entityType = context.entityType.shortName;
-						var permissionsKey = entityType + "-" + context.entityAspect.getKey().toJSON().values.toString();
-						var permissionArray = contextPermissions[permissionsKey];
-
-						if (angular.isUndefined(permissionArray)) {
-							throw Error("Permissions for the given context are undefined.");
-						}
-
-						return arrayHasString(permissionArray, permission);
-					}
-
-					return arrayHasString(contextlessPermissions, permission);
-				},
-				getContextPermissions: function (context, forceRefresh) {
-					var entityType = context.entityType.shortName;
-					var permissionsKey = entityType + "-" + context.entityAspect.getKey().toJSON().values.toString();
-
-					var pendingPromise = pendingPromises[permissionsKey];
-					if (angular.isDefined(pendingPromise) && pendingPromise != null) {
-						return pendingPromise;
-					}
-
-					var deferred = $q.defer();
-
-					var permissionArray = contextPermissions[permissionsKey];
-
-					if (forceRefresh || angular.isUndefined(permissionArray)) {
-						pendingPromises[permissionsKey] = deferred.promise;
-
-						var handler = handlers[entityType];
-
-						if (handler) {
-							handler.then(function (response) {
-								var permissions = [];
-								for (var i = 0; i < response.length; ++i) {
-									permissions.push(response[i].Name);
-								}
-
-								contextPermissions[permissionsKey] = permissions;
-								deferred.resolve();
-								pendingPromises[permissionsKey] = null;
-							});
-						} else {
-							throw Error("Permission checks are not supported for entities of type " + entityType + ".");
-						}
-					} else {
-						deferred.resolve();
-					}
-
-					return deferred.promise;
+				if (angular.isUndefined(permissionArray)) {
+					throw Error("Permissions for the given context are undefined.");
 				}
-			};
-		}];
-	});
+
+				return arrayHasString(permissionArray, permission);
+			}
+
+			return arrayHasString(contextlessPermissions, permission);
+		};
+
+		this.getContextPermissions = function(context, forceRefresh) {
+			var entityType = context.entityType.shortName;
+			var permissionsKey = entityType + "-" + context.entityAspect.getKey().toJSON().values.toString();
+
+			var pendingPromise = pendingPromises[permissionsKey];
+			if (angular.isDefined(pendingPromise) && pendingPromise != null) {
+				return pendingPromise;
+			}
+
+			var deferred = $q.defer();
+
+			var permissionArray = contextPermissions[permissionsKey];
+
+			if (forceRefresh || angular.isUndefined(permissionArray)) {
+				pendingPromises[permissionsKey] = deferred.promise;
+
+				var handler = handlers[entityType];
+
+				if (handler) {
+					handler(context).then(function(response) {
+						var permissions = [];
+						for (var i = 0; i < response.length; ++i) {
+							permissions.push(response[i].Name);
+						}
+
+						contextPermissions[permissionsKey] = permissions;
+						deferred.resolve();
+						pendingPromises[permissionsKey] = null;
+					});
+				} else {
+					throw Error("Permission checks are not supported for entities of type " + entityType + ".");
+				}
+			} else {
+				deferred.resolve();
+			}
+
+			return deferred.promise;
+		};
+	}]);
