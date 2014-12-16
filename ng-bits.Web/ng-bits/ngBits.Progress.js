2434f61ad5cf91297e24e7734106491c5e4e1ae4
niaher <niaher.crimston@gmail.com>
2014-12-16 19:06:07 +0700

    Create ngBits.Progress.js

---
+++ ng-bits.Web/ng-bits/ngBits.Progress.js
@@ -0,0 +1,47 @@
+// Uses ngProgress (https://github.com/VictorBjelkholm/ngProgress)
+// to show loading indicator for each ajax/full-postback request.
+
+app.ng.config(["$httpProvider", function ($httpProvider) {
+	$httpProvider.interceptors.push(["$injector", function ($injector) {
+		var completeProgress, getNgProgress, ngProgress, working;
+		working = false;
+
+		getNgProgress = function () {
+			ngProgress = ngProgress || $injector.get("ngProgress");
+			ngProgress.color("rgb(197, 30, 43)");
+			return ngProgress;
+		};
+
+		completeProgress = function () {
+			if (working) {
+				var ngProgressInstance = getNgProgress();
+				ngProgressInstance.complete();
+				working = false;
+			}
+		};
+
+		return {
+			request: function (request) {
+				if (!working) {
+					var ngProgressInstance = getNgProgress();
+					ngProgressInstance.reset();
+					ngProgressInstance.start();
+					working = true;
+				}
+				return request;
+			},
+			requestError: function (request) {
+				completeProgress();
+				return request;
+			},
+			response: function (response) {
+				completeProgress();
+				return response;
+			},
+			responseError: function (response) {
+				completeProgress();
+				return response;
+			}
+		}
+	}]);
+}]);
